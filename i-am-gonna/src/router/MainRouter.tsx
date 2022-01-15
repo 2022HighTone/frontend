1f43be52e4ac7c1d5fdd1fa8f805a1f5bde1e8c2
hyejun <hyejun0608@naver.com>
2022-01-15 15:44:57 +0900

    ⚙️:: router

---
+++ i-am-gonna/src/router/MainRouter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const MainRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>{/* <Route path='/' element={} /> */}</Routes>
+    </BrowserRouter>
+  );
+};
+
+export default MainRouter;
