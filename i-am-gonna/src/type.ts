4ad65bde26707425e725fa8d4677e5e3e37b7214
hyejun <hyejun0608@naver.com>
2022-01-16 03:49:01 +0900

    ⚡️:: type

--- i-am-gonna/src/type.ts
+++ i-am-gonna/src/type.ts
@@ -3,3 +3,32 @@ export type error = {
   message: string;
   type: string;
 };
+
+export interface responseGenerator {
+  config?: any;
+  data?: any;
+  headers?: any;
+  request?: any;
+  status?: number;
+  statusText?: string;
+}
+
+export type searchResponse = {
+  id: number;
+  name: string;
+  address: string;
+  latitude: number;
+  longitude: number;
+  is_default: boolean;
+};
+
+export type signupRequest = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export type loginRequest = {
+  email: string;
+  password: string;
+};
