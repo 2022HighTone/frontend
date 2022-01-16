9216c53eb1d0c813ac52397f9d75d8daaabcddc3
hyejun <hyejun0608@naver.com>
2022-01-16 12:48:07 +0900

    ⚡️:: mainRouter

--- i-am-gonna/src/router/MainRouter.tsx
+++ i-am-gonna/src/router/MainRouter.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from '../components/main';
+import Map from '../components/maps';
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
-      <Routes>{/* <Route path='/' element={} /> */}</Routes>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/map' element={<Map />} />
+      </Routes>
     </BrowserRouter>
   );
 };
