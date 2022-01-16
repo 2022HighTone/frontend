7bd45a04ac6c4025f3b61f498b584ada48fa685a
hyejun <hyejun0608@naver.com>
2022-01-16 12:47:03 +0900

    ⚡️:: 로그인 & 회원가입 화면에 예쁜 사진 넣기

--- i-am-gonna/src/components/main/index.tsx
+++ i-am-gonna/src/components/main/index.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 import * as S from './style';
 import Login from '../login';
 import Signup from '../signup';
+import { artboard } from '../../assests/signup';
 
-const Main = () => {
+interface Props {}
+
+const Main: FC<Props> = props => {
   const [isLogin, setIsLogin] = useState<boolean>(true);
 
   return (
     <S.Main>
+      <S.MainImg src={artboard} />
       {isLogin && <Login setIsLogin={setIsLogin} />}
       {!isLogin && <Signup />}
     </S.Main>
