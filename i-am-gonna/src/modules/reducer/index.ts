2a8ac07cba7020084ef26693a4beb02ff0b442e7
hyejun <hyejun0608@naver.com>
2022-01-16 12:48:33 +0900

    ⚡️:: rootReducer

--- i-am-gonna/src/modules/reducer/index.ts
+++ i-am-gonna/src/modules/reducer/index.ts
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
 import LoginReducer from './login';
+import MapReducer from './map';
+import SearchReducer from './search';
 import SignupReducer from './signup';
 
 const rootReducer = combineReducers({
   login: LoginReducer,
   signup: SignupReducer,
+  search: SearchReducer,
+  map: MapReducer,
 });
 
 export type reducerType = ReturnType<typeof rootReducer>;
