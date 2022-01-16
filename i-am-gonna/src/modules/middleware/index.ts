8ba9d254361b42b5cd4c20ece586b5d626fd6b5a
hyejun <hyejun0608@naver.com>
2022-01-16 12:49:01 +0900

    ⚡️:: rootSaga

--- i-am-gonna/src/modules/middleware/index.ts
+++ i-am-gonna/src/modules/middleware/index.ts
@@ -1,7 +1,9 @@
 import { all } from 'redux-saga/effects';
 import loginSaga from './login';
+import mapSaga from './map';
+import searchSaga from './search';
 import signupSaga from './signup';
 
 export default function* rootSata() {
-  yield all([signupSaga(), loginSaga()]);
+  yield all([signupSaga(), loginSaga(), searchSaga(), mapSaga()]);
 }
